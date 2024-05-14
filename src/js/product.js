74302e528f120ce7800deb40521da50698fac502
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2024-05-14 16:50:11 -0500

    .

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,20 @@
 import { setLocalStorage } from "../js/utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
+loadHeaderFooter();
+
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -15,15 +25,3 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-  
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
-  
-loadHeaderFooter();
-  
-const dataSource = new ProductData("tents");
-const productId = getParam("product");
-  
-const product = new ProductDetails(productId, dataSource);
-product.init();
